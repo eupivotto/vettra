f53b9a7f1d1b361cc1d6d4a50ac2413f4964ce61
eupivotto <agenciadigi62@gmail.com>
2025-08-25 08:56:12 -0400

    adicinando conteudo simples na página serviços para deploy

---
+++ src/app/servicos/page.tsx
@@ -0,0 +1,10 @@
+// src/app/servicos/page.tsx
+
+export default function ServicosPage() {
+  return (
+    <div>
+      <h1>Nossos Serviços</h1>
+      <p>Conteúdo da página de serviços virá aqui.</p>
+    </div>
+  );
+}
